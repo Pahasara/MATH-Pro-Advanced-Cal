7e472522d21a5fd664290a443e677121270774f3
Pahasara Dewnith Fernando <46932317+Pahasara@users.noreply.github.com>
2020-06-01 07:00:16 +0530

    DvNET MATH PRO v1.3 UPDATE

--- Main.cs
+++ Main.cs
@@ -1,11 +1,12 @@
 ﻿/*
  *
- * Copyright(c) 2019, M.P.Dewnith Fernando and/or its affiliates.All rights reserved.
+ * Copyright(c) 2019, 2020, M.P.Dewnith Fernando and/or its affiliates.All rights reserved.
  * M.P.Dewnith Fernando PROPRIETARY/CONFIDENTIAL.Use is subject to license terms.
  * 
  */
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,21 +15,30 @@ namespace DvNET.MATH.Core
 {
     public class Main
     {
+        public string GetVersion()
+        {
+            return "v1.3";
+        }
+
+        public string GetYear()
+        {
+            return "2020";
+        }
+
+        public string GetBuild()
+        {
+            return "2005";
+        }
+
         public string InputTextError()
         {
-            string error = "Error : Please Enter valid values!";
+            string error = "Error : Please enter only the valid values!";
             return error;
         }
         public string InputSubmitError()
         {
-            string error = "Error : Please Enter the valid values, before calculating.";
+            string error = "Error : Please enter the valid values, before calculation....";
             return error;
         }
-        public string PythagoreanSubmitError()
-        {
-            string error = "Error : Please Enter the valid values for calculating.";
-            return error;
-        }
-
     }
 }
--- Scientific.cs
+++ Scientific.cs
@@ -1,6 +1,6 @@
 ﻿/*
  *
- * Copyright(c) 2019, M.P.Dewnith Fernando and/or its affiliates.All rights reserved.
+ * Copyright(c) 2019, 2020, M.P.Dewnith Fernando and/or its affiliates.All rights reserved.
  * M.P.Dewnith Fernando PROPRIETARY/CONFIDENTIAL.Use is subject to license terms.
  * 
  */
@@ -14,6 +14,8 @@ namespace DvNET.MATH.Core
 {
     public class Scientific
     {
+        double Pi = 22 / 7;
+
         public Double Speed(double Distance, double Time)
         {
             return Distance / Time;
@@ -22,9 +24,9 @@ namespace DvNET.MATH.Core
         {
             return Extended / Time;
         }
-        public Double Acceleration(double Velocity, double Time)
+        public Double Acceleration(double U,double V, double Time)
         {
-            return Velocity / Time;
+            return (V-U) / Time;
         }
         public Double Power(double Energy, double Time)
         {
@@ -39,14 +41,42 @@ namespace DvNET.MATH.Core
             return Speed * Time;
         }
 
-        public Double Energy(double Mass, double Velocity)
+        public Double Energy(double Force, double Distance)
         {
-            return 0.5 * Mass * Velocity * Velocity;
+            return Force * Distance;
         }
         public Double Force(double Mass, double Gravity)
         {
             return Mass * Gravity;
         }
 
+        public Double RForce(double P, double Q, double T)
+        {
+            T = T * Pi / 180;
+            double R = Math.Sqrt((P * P) + (Q * Q) + (2 * P * Q * Math.Cos(T)));
+            return R;
+        }
+
+        public Double LinearKineticEnergy(double Mass,double Velocity)
+        {
+            double Energy = 0.5 * Mass * Velocity * Velocity;
+            return Energy;
+        }
+
+        public Double GravPotentialEnergy(double Mass, double Gravity, double Height)
+        {
+            double Energy = Mass * Gravity * Height;
+            return Energy;
+        }
+
+        public Double AngularV(double Frequency)
+        {
+            return 2 * Pi * Frequency;
+        }
+
+        public Double Frequency(double Rounds, double Time)
+        {
+            return Rounds / Time;
+        }
     }
 }
